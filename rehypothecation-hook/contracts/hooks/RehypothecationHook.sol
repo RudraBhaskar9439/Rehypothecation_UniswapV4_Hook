03b0a88e82398effb85f1e7e1bd46c3a3aba791a
Rudra Bhaskar <rbrudra9439@gmail.com>
2025-09-02 09:53:14 +0530

    Initializing the constants for the contract and initializing the contract.

---
+++ rehypothecation-hook/contracts/hooks/RehypothecationHook.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import {IRehypothecationHook} from "../interfaces/IRehypothecationHook.sol";
+import {IAave} from "../interfaces/IAave.sol";
+import {Constants} from "../utils/Constants.sol";
+
+abstract contract RehypothecationHook is IRehypothecationHook {
+    // State Vairable 
+
+
+    // Ownership initialization
+
+
+    // Events
+
+    
+}
--- rehypothecation-hook/contracts/utils/Constants.sol
+++ rehypothecation-hook/contracts/utils/Constants.sol
@@ -5,4 +5,13 @@ pragma solidity ^0.8.26;
 library Constant {
     uint8 public constant DEFAULT_RESERVE_PCT = 20;
     uint256 public constant MIN_POSITION_SIZE = 1e15;
+
+    // Reserve Percentages
+    uint256 public constant RESERVE_PERCENTAGE_DEFAULT = 20;   // 20% RESERVE
+    uint256 public constant MIN_RESERVE_PERCENTAGE = 10;  // 10% minimum
+    uint256 public constant MAX_RESERVE_PERCENTAGE = 50;  // 50% MAXIMUM
+
+    // Emergency withdrawal
+    uint256 public constant EMERGENCY_WITHDRAWAL_DELAY = 1 hours;
 }
+
